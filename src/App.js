9af0368e23f764fc5760273ae8505316cf54a7da
lighty <hikalin8686@gmail.com>
2021-05-09 01:46:58 +0900

    projectsの追加フォームを作る

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/entries">Entries</Link>
+            </li>
+            <li>
+              <Link to="/tasks">Tasks</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/entries">
+            <Entries />
+          </Route>
+          <Route path="/tasks">
+            <Tasks />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Entries() {
+  return <h2>Home</h2>;
+}
+
+function Tasks() {
+  return <h2>About</h2>;
+}
+
+class Projects extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      projects: [
+        { name: 'reactの勉強' },
+        { name: 'typescriptの勉強' },
+      ],
+      message: '',
+    }
+  }
+
+  handleSubmit(name) {
+    const projects = this.state.projects.concat([{name: name}]);
+    this.setState({projects: projects, message: '登録しました'});
+  }
+
+  render() {
+    const projects = this.state.projects.map((project) => <li key={project.name}>{project.name}</li>);
+    return (
+      <div>
+        <h2>Projects</h2>
+        <p>{this.state.message}</p>
+        <div><ProjectForm onSubmit={this.handleSubmit}/></div>
+        <ul>{projects}</ul>
+      </div>
+    );
+  }
+}
+
+// 制御されたコンポーネントを書いてみる
+class ProjectForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''}
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.onSubmit(this.state.value);
+    this.setState({value: ''});
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type='text' value={this.state.value} onChange={this.handleChange} />
+        <input type='submit' value='Submit' />
+      </form>
+    );
+  }
+}
