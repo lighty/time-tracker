3a2bfcb67a1def5e960ab528734f7618446292f7
lighty <hikalin8686@gmail.com>
2021-05-09 12:07:01 +0900

    関数コンポーネントに書き換える

--- src/Projects.js
+++ src/Projects.js
@@ -1,84 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default class Projects extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleAddProject = this.handleAddProject.bind(this);
-    this.handleDeleteProject = this.handleDeleteProject.bind(this);
-    this.state = {
-      projects: [
-        { name: 'reactの勉強' },
-        { name: 'typescriptの勉強' },
-      ],
-      message: '',
-    }
+export default function Projects() {
+  const initialProjects = [
+    { name: 'reactの勉強' },
+    { name: 'typescriptの勉強' },
+  ];
+  const [projects, setProjects] = useState(initialProjects);
+  const [message, setMessage] = useState('');
+
+  const handleAddProject = (name) => {
+    setProjects(projects.concat([{name: name}]));
+    setMessage(`${name}を追加しました`);
+  }
+  const handleDeleteProject = (name) => {
+    setProjects(projects.filter((p) => p.name !== name));
+    setMessage(`${name}を削除しました`);
   }
 
-  handleAddProject(name) {
-    const projects = this.state.projects.concat([{name: name}]);
-    this.setState({projects: projects, message: `${name}を追加しました`});
-  }
+  const project_components = projects.map((project) => {
+    return <li key={project.name}>{project.name} <DeleteForm onClick={handleDeleteProject} name={project.name}/></li>;
+  });
 
-  handleDeleteProject(name) {
-    const newProjects = this.state.projects.filter((e) => e.name !== name);
-    this.setState({projects: newProjects, message: `${name}を削除しました`});
-  }
-
-  render() {
-    const projects = this.state.projects.map((project) => {
-      return <li key={project.name}>{project.name} <DeleteForm onClick={this.handleDeleteProject} name={project.name}/></li>;
-    });
-    return (
-      <div>
-        <h2>Projects</h2>
-        <p>{this.state.message}</p>
-        <div><AddForm onSubmit={this.handleAddProject}/></div>
-        <ul>{projects}</ul>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h2>Projects</h2>
+      <p>{message}</p>
+      <div><AddForm onSubmit={handleAddProject}/></div>
+      <ul>{project_components}</ul>
+    </div>
+  );
 }
 
-class AddForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''}
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+function AddForm(props){
+  const [value, setValue] = useState('')
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
+  const handleChange = (event) => setValue(event.target.value);
+  const handleSubmit = (event) => {
     event.preventDefault();
-    this.props.onSubmit(this.state.value);
-    this.setState({value: ''});
+    props.onSubmit(value);
+    setValue('');
   }
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <input type='text' value={this.state.value} onChange={this.handleChange} />
-        <input type='submit' value='追加' />
-      </form>
-    );
-  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type='text' value={value} onChange={handleChange} />
+      <input type='submit' value='追加' />
+    </form>
+  );
 }
 
-class DeleteForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(event) {
+function DeleteForm(props) {
+  const handleClick = (event) => {
     event.preventDefault();
-    this.props.onClick(this.props.name);
+    props.onClick(props.name);
   }
 
-  render() {
-    return <button onClick={this.handleClick}>削除</button>;
-  }
+  return <button onClick={handleClick}>削除</button>;
 }
