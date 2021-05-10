7ca2c7127fbec8db1eb525fa191f44e7199b566a
lighty <hikalin8686@gmail.com>
2021-05-10 14:00:37 +0900

    taskの表示

--- src/Tasks.js
+++ src/Tasks.js
@@ -1,28 +1,70 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
+
+const initTaskState = {
+  tasks: [
+    {name: 'Taskページを作る', projectName: 'TimeTrackerの開発'},
+  ],
+  messsage: '',
+};
+
+const init = (initialArg) => {
+  const tasks = localStorage.getItem('tasks')
+  if (tasks) {
+    return { tasks: JSON.parse(tasks), message: '' };
+  } else {
+    return initialArg;
+  }
+}
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'addTask':
+      const newTasks = state.tasks.concat([{name: action.name, projectName: action.projectName}]);
+      localStorage.setItem('tasks', JSON.stringify(newTasks));
+      return {
+        tasks: newTasks,
+        message: `${action.name}を追加しました`,
+      }
+    default:
+      throw new Error();
+  }
+}
 
 const Tasks = () => {
+  const [state, dispatch] = useReducer(reducer, initTaskState, init);
+  const tasks = state.tasks.map(task => <li key={task.name}>{task.name} [{task.projectName}]</li>);
+  const addTask = (name, projectName) => {
+    dispatch({type: 'addTask', name: name, projectName: projectName});
+  }
   return (
     <div>
       <h2>Tasks</h2>
-      <p>/* state.message */</p>
-      <div><AddForm/></div>
+      <p>{state.message}</p>
+      <div><AddForm onSubmit={addTask}/></div>
+      <ul>{tasks}</ul>
     </div>
   );
 }
 
 const AddForm = (props) => {
   const [name, setName] = useState('');
-  const [projectName, setProjectName] = useState('');
   const projects = JSON.parse(localStorage.getItem('projects'));
+  const [projectName, setProjectName] = useState(projects[0].name);
   const projectOptions = projects.map(project => <option value={project.name}>{project.name}</option>);
 
   const handleNameChange = e => setName(e.target.value);
   const handleProjectNameChange = e => setProjectName(e.target.value);
+  const handleSubmit = e => {
+    e.preventDefault();
+    props.onSubmit(name, projectName);
+    setName('');
+    setProjectName('');
+  }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input type='text' value={name} onChange={handleNameChange} />
-      <select onChange={handleProjectNameChange}>
+      <select onChange={handleProjectNameChange} value={projectName}>
         {projectOptions} 
       </select>
       <input type='submit' value='追加' />
